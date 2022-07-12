ef4768f74242490ddd7254f06ed5eb6449ff7d7f
Jyoti Singh <jyotisinghva@users.noreply.github.com>
2022-07-12 20:28:26 +0000

    While looping

--- cust_if_script.py
+++ cust_if_script.py
@@ -5,15 +5,19 @@
 
 message = "Your grade is"
 
-# wrap connection in a float() to accept decimals as numbers
+# wrap score in a int() to accept numbers
 score = int(input("What is your score : "))
 
 if score >= 90 and score <= 100:
    grade = 'A'
-elif score >= 80 and score < 90:
+elif score >= 80:
    grade = 'B'
+elif score >= 70: 
+   grade = 'C'
+elif score >= 60:
+   grade = 'D'
 else:
    grade = 'F'
 
-print(message , grade)
+print(f"Your grade is {grade}")
 
