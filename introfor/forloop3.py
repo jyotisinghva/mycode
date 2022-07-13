201f08ec74678e327fbdc63967dfecadf5170f9c
Jyoti Singh <jyotisinghva@users.noreply.github.com>
2022-07-13 15:42:34 +0000

    Looping techniques

---
+++ introfor/forloop3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
+for farm_list in farms:
+    print ("list is ", farm_list)
---
+++ test.py
@@ -0,0 +1,5 @@
+you_say = {"an" : 1, "ab": 2}
+
+
+you_say["ac"] = 8
+print(  you_say )
